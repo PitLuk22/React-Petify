e326b1b9854633d2916a270e7cdc19bae6eaddf0
Petr <pitluk22@gmail.com>
2021-01-18 22:18:51 +0300

    add extra player settings

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,10 +1,11 @@
 import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStepBackward, faStepForward, faVolumeUp, faVolumeMute, faVolumeDown } from '@fortawesome/free-solid-svg-icons';
+import { faStepBackward, faStepForward, faVolumeUp, faVolumeMute, faVolumeDown, faRandom } from '@fortawesome/free-solid-svg-icons';
 import { faPlayCircle, faPauseCircle } from '@fortawesome/free-regular-svg-icons';
 import Like from '../like';
 
 import { playAudio } from '../../util';
+
 
 const Player = ({ songs, currentSong, setCurrentSong, isPlaying, setIsPlaying, setSongs }) => {
 
@@ -15,6 +16,8 @@ const Player = ({ songs, currentSong, setCurrentSong, isPlaying, setIsPlaying, s
 		duration: 0,
 		'volume': volume
 	})
+	const [repeat, setRepeat] = useState(true);
+	const [random, setRandom] = useState(false);
 
 	const audioRef = useRef(null);
 
@@ -64,11 +67,12 @@ const Player = ({ songs, currentSong, setCurrentSong, isPlaying, setIsPlaying, s
 	}
 
 	const nextSong = (n) => {
+		console.log(n);
 		const index = songs.findIndex(song => song.active);
 
-		// define next track
+		// define next track 
 		let nextSong;
-		if (n > 0) {
+		if (n >= 0) {
 			nextSong = songs[(index + n) % songs.length];
 		} else {
 			nextSong = index + n < 0 ? songs[songs.length - 1] : songs[index + n];
@@ -153,9 +157,32 @@ const Player = ({ songs, currentSong, setCurrentSong, isPlaying, setIsPlaying, s
 		else return faVolumeMute
 	}
 
+	const setExtraSettingsHandler = (selectedSetting) => {
+		switch (selectedSetting) {
+			case 'rnd':
+				setRandom(!random)
+				if (repeat) setRepeat(false);
+				break;
+			case 'rep':
+				setRepeat(!repeat)
+				if (random) setRandom(false);
+				break;
+			default: return;
+		}
+	}
+
 	return (
 		<div className='player'>
 			<div className='control'>
+				<div
+					className={`control__random ${random ? 'dot' : ''}`}
+					onClick={() => setExtraSettingsHandler('rnd')}>
+					<FontAwesomeIcon
+						size='sm'
+						icon={faRandom}
+						style={{ color: random ? '#1db954' : '#b3b3b3' }}
+					/>
+				</div>
 				<FontAwesomeIcon
 					onClick={() => nextSong(-1)}
 					className='control__prev'
@@ -170,7 +197,28 @@ const Player = ({ songs, currentSong, setCurrentSong, isPlaying, setIsPlaying, s
 					onClick={() => nextSong(1)}
 					className='control__next'
 					size='lg'
-					icon={faStepForward} />
+					icon={faStepForward}
+				/>
+				<div
+					className={`control__repeat ${repeat ? 'dot' : ''}`}
+					onClick={() => setExtraSettingsHandler('rep')}>
+					<svg
+						style={{ fill: repeat ? '#1db954' : '#b3b3b3' }}
+						viewBox="0 0 512.016 512.016">
+						<g>
+							<path d="M507.336,100.696l-96-96c-4.576-4.576-11.456-5.952-17.44-3.456c-5.984,2.496-9.888,8.288-9.888,14.752v48h-208
+								c-97.216,0-176,78.784-176,176c0,8.832,7.168,16,16,16h64c8.832,0,16-7.168,16-16c0-44.192,35.808-80,80-80h208v48
+								c0,6.464,3.904,12.32,9.888,14.784c5.984,2.496,12.864,1.12,17.44-3.456l96-96C513.576,117.08,513.576,106.936,507.336,100.696z"
+							/>
+						</g>
+						<g>
+							<path d="M496.008,255.992h-64c-8.832,0-16,7.168-16,16c0,44.192-35.808,80-80,80h-208v-48c0-6.464-3.904-12.32-9.888-14.784
+								s-12.832-1.088-17.44,3.488l-96,96c-6.24,6.24-6.24,16.384,0,22.624l96,96c4.576,4.576,11.456,5.952,17.44,3.456
+								s9.888-8.32,9.888-14.784v-48h208c97.216,0,176-78.784,176-176C512.008,263.16,504.84,255.992,496.008,255.992z"/>
+
+						</g>
+					</svg>
+				</div>
 			</div>
 			<div className='timeline'>
 				<div className="volume">
@@ -195,7 +243,7 @@ const Player = ({ songs, currentSong, setCurrentSong, isPlaying, setIsPlaying, s
 					min='0'
 					max={songInfo.duration || 0}
 					value={songInfo.currentTime} />
-				<p className='timeline__end'>{songInfo.duration ? getTime(songInfo.duration) : '0:00'}</p>
+				<p className='timeline__end'>{songInfo.duration ? getTime(songInfo.duration) : `0:00`}</p>
 				<Like
 					setLikeHandler={(e, toggle) => setLikeHandler(e, toggle)}
 					currentSong={currentSong} />
@@ -206,8 +254,12 @@ const Player = ({ songs, currentSong, setCurrentSong, isPlaying, setIsPlaying, s
 				onLoadedMetadata={timeUpdateHandler}
 				ref={audioRef}
 				src={currentSong.audio}
-				onEnded={() => nextSong(1)} />
-		</div>
+				onEnded={() => {
+					if (random) nextSong(Math.floor(Math.random() * (songs.length - 1) + 1))
+					else if (repeat) nextSong(0)
+					else nextSong(1)
+				}} />
+		</div >
 	)
 }
 
